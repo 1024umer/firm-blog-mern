9547efc9872c7aa64656fe8e91bd4aea9f7de2e9
1024umer <127780703+1024umer@users.noreply.github.com>
2024-01-17 00:13:11 +0500

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
 function App() {
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      app
-    </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
