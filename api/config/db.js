d03324eed022a5ec1d3a56e849b252a974214375
1024umer <127780703+1024umer@users.noreply.github.com>
2024-01-17 01:32:45 +0500

    fixes

--- api/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = ()=>{
-    mongoose.connect(process.env.MONGO_URL).then
-}
-module.exports = connectDB
--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
-import connectDB from './config/db'
-connectDB()
-const app = express();
 import dotenv  from 'dotenv'
+import mongoose from 'mongoose';
+import colors from 'colors'
+dotenv.config()
+const app = express();
 const port = process.env.PORT || 5000;
 
+
+const connectDB = async () => {
+    try {
+       const conn =  await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB connected ${conn.connection.host}`.green.bold.underline);
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit with an error code
+    }
+};
+connectDB()
 app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`.yellow.bold.underline)
 })
