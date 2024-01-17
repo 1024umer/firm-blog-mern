304b7943100a936d3bd868a2fd384014166f2144
1024umer <127780703+1024umer@users.noreply.github.com>
2024-01-18 03:17:30 +0500

    signup complete

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,8 +4,7 @@ import { errorHandler } from '../utils/error.js';
 export const signup = async(req,res,next)=>{
     const {username,email,password} = req.body;
     if(!username || !email || !password && username == '' || email == '' || password == ''){
-        res.status(404)
-        res.send('All fields are required')
+        next(errorHandler(400, 'All fields are required'));
     }
     const hashedPassword = bcrypt.hashSync(password,10);
     try {
