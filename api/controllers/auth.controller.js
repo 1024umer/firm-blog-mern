fc3d1f8790e18c969b36776573075130561d6d7e
1024umer <127780703+1024umer@users.noreply.github.com>
2024-01-18 01:48:09 +0500

    auth fixes and error handling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,17 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
-export const signup = async(req,res)=>{
+import { errorHandler } from '../utils/error.js';
+export const signup = async(req,res,next)=>{
     const {username,email,password} = req.body;
     if(!username || !email || !password && username == '' || email == '' || password == ''){
         res.status(404)
         res.send('All fields are required')
     }
-    const hashedPassword = bcrypt.hashSync(password,10)
-    const user = await User.create({username,email,password:hashedPassword})
-    if(user){
-        res.status(200).send({user})
-    }else{
-        res.status(404)
-        throw new Error('There is some error')
-    }
+    const hashedPassword = bcrypt.hashSync(password,10);
+    try {
+        const user = await User.create({username,email,password:hashedPassword})
+        res.json('Signup successful');
+      } catch (error) {
+        next(error);
+      }
 }
