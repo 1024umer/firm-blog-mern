09a03f3d5bede183be6db624497397ebc8a661ff
1024umer <127780703+1024umer@users.noreply.github.com>
2024-01-19 23:50:13 +0500

    fixes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -29,7 +29,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, 'Invalid password'));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+    const token = jwt.sign({ id: validUser._id,isAdmin:validUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' })
     const { password: pass, ...rest } = validUser._doc
     res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest)
   } catch (error) {
@@ -41,7 +41,7 @@ export const google = async (req,res,next)=>{
   try {
     const user = await User.findOne({email});
     if(user){
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+    const token = jwt.sign({ id: user._id,isAdmin:user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' })
     const { password, ...rest } = user._doc
     res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest)
     }else{
