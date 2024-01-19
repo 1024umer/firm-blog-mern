20051c4c70d03a703eb8363e9e21649c0ff04e31
1024umer <127780703+1024umer@users.noreply.github.com>
2024-01-20 02:28:44 +0500

    get post api and route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createPost } from '../controllers/post.controller.js';
+import { createPost,getposts } from '../controllers/post.controller.js';
 import { verify } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.route('/create').post(verify,createPost)
+router.route('/getPost').get(verify,getposts)
 
 export default router
